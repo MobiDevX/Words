25adf8f847def8db64927dcfa7f99acb9c188cf3
MobDe <MobDevX.outlook.com>
2022-06-16 10:27:44 +0700

    Change words table

--- app/src/main/java/e/words/db/Word.kt
+++ app/src/main/java/e/words/db/Word.kt
@@ -4,4 +4,4 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "word_table")
-data class Word(@PrimaryKey val word: String)
+data class Word(@PrimaryKey val word: String, val rus: String)
--- app/src/main/java/e/words/db/WordDao.kt
+++ app/src/main/java/e/words/db/WordDao.kt
@@ -1,9 +1,6 @@
 package e.words.db
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface WordDao {
@@ -11,8 +8,14 @@ interface WordDao {
     @Query("SELECT * FROM word_table ORDER BY word ASC")
     fun getAlphabetizedWords(): List<Word>
 
+    @Update()
+    fun update(word: Word)
+
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(word: Word)
+
+    @Delete()
+    fun delete(word: Word)
 
     @Query("DELETE FROM word_table")
     fun deleteAll()
