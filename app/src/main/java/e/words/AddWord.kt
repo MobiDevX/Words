0c6e3839171835f9643a5b7401fd4bb30919c929
MobDe <MobDevX.outlook.com>
2022-06-17 11:46:41 +0700

    Add Word Activity

---
+++ app/src/main/java/e/words/AddWord.kt
@@ -0,0 +1,11 @@
+package e.words
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddWord : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_word)
+    }
+}
